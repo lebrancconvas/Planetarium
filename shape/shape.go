6b7bb78fb77d64f1457ec0d435fce5f948887351
lebrancconvas <lebrancconvas@gmail.com>
2022-09-20 20:16:50 +0700

    🎨 refactor: change all field to uppercase

--- shape/shape.go
+++ shape/shape.go
@@ -10,41 +10,41 @@ type ShapeCalculation interface {
 }
 
 type Rectangle struct {
-	width  float64
-	height float64
+	Width  float64
+	Height float64
 }
 
 type Circle struct {
-	radius float64
+	Radius float64
 }
 
 type Triangle struct {
-	aWidth float64
-	bWidth float64
-	cWidth float64
+	AWidth float64
+	BWidth float64
+	CWidth float64
 }
 
 func (r *Rectangle) Circumstance() float64 {
-	return (r.width + r.height) * 2;
+	return (r.Width + r.Height) * 2;
 }
 
 func (r *Rectangle) Area() float64 {
-	return r.width * r.height;
+	return r.Width * r.Height;
 }
 
 func (c *Circle) Circumstance() float64 {
-	return 2 * math.Pi * c.radius;
+	return 2 * math.Pi * c.Radius;
 }
 
 func (c *Circle) Area() float64 {
-	return math.Pi * c.radius * c.radius;
+	return math.Pi * c.Radius * c.Radius;
 }
 
 func (t *Triangle) Circumstance() float64 {
-	return t.aWidth + t.bWidth + t.cWidth;
+	return t.AWidth + t.BWidth + t.CWidth;
 }
 
 func (t *Triangle) Area() float64 {
-	s := (t.aWidth + t.bWidth + t.cWidth) / 2;
-	return math.Sqrt(s * (s - t.aWidth) * (s - t.bWidth) * (s - t.cWidth));
+	s := (t.AWidth + t.BWidth + t.CWidth) / 2;
+	return math.Sqrt(s * (s - t.AWidth) * (s - t.BWidth) * (s - t.CWidth));
 }
